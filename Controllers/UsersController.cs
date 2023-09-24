2e68500e0b66cc626be31df6ee58456039a1ffd5
unknown <mateo.ferrero@cablasociados.com>
2023-09-24 18:50:11 -0300

    Agrego controlador UserModel

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GIT_MVC.Models;
+
+namespace GIT_MVC.Controllers
+{
+	[Route("users")]
+	public class UsersController : Controller
+	{
+		[Route ("obtener-todos")]
+		public IActionResult GetList()
+		{
+			var user = new UserModel
+			{
+				Name = "Mateo",
+				LastName = "Ferrero"
+			};
+
+			return View("Index", user);
+		}
+	}
+}
